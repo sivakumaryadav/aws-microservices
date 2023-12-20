977bd59cedf25898a3f93b4d66ea0bf141389672
siva <sivakumarthota50@gmail.com>
2023-12-20 19:34:46 +0530

    Initial Commit of Config Server.

---
+++ eureka-config-server/src/test/java/com/aws/eureka/config/server/EurekaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.eureka.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
