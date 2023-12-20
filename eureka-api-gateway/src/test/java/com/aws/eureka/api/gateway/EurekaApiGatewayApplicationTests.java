63475b48555350da2da73ec67e20e55b1f8e4b52
siva <sivakumarthota50@gmail.com>
2023-12-20 10:02:28 +0530

    New code checkin

---
+++ eureka-api-gateway/src/test/java/com/aws/eureka/api/gateway/EurekaApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.aws.eureka.api.gateway;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EurekaApiGatewayApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
